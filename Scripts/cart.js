3b6cb8985b11314a5adfc346da1d456a04ea9ab7
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2023-10-15 14:59:38 -0700

    Removed toastBtn

--- Scripts/cart.js
+++
@@ -1,9 +0,0 @@
-const toastTrigger = document.getElementById('cartToast')
-const toastCart = document.getElementById('liveToast')
-
-if (toastTrigger) {
-  const toastAlert = bootstrap.Toast.getOrCreateInstance(toastCart)
-  toastTrigger.addEventListener('click', () => {
-    toastAlert.show()
-  })
-}
